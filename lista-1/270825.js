ead5a478cb7a242b0a8ac579b20ec0a1fa7436b5
Miguel do Nascimento Evangelista <miguel.evangelista4@etec.sp.gov.br>
2025-08-27 23:44:39 +0000

    okk

--- lista-1/270825.js
+++ lista-1/270825.js
@@ -2,7 +2,7 @@ let input = require('readline-sync');
 let divisor = input.questionInt("Digite o divisor: ");
 let dividendo = input.questionInt("Digite o dividendo: ");
 let quociente = dividendo/divisor
-let resto = quociente-parseInt(quociente)
+let resto = parseInt(quociente) 
+let resposta = resto - quociente 
 
-
-console.log("Resposta=", resto)
+console.log("Resposta=", resposta)
