2c8b6bfbe08c731b60599e9f0875b9fb5a4fd528
Miguel do Nascimento Evangelista <miguel.evangelista4@etec.sp.gov.br>
2025-09-29 21:45:14 +0000

    faff

--- lista-4/ex4.js
+++ lista-4/ex4.js
@@ -3,6 +3,6 @@ let n1 = input.questionInt("Digite um numero: ")
 let n2 = input.questionInt("Digite um numero: ")
 let n3 = input.questionInt("Digite um numero: ")
 
-if (n1>n2, n1>n3) { console.log(n1)}
-if (n2>n1, n2>n3) { console.log(n2)}
-if (n3>n1, n3>n2) { console.log(n3)}
+if (n1>n2 && n1>n3) { console.log(n1)}
+if (n2>n1 && n2>n3) { console.log(n2)}
+if (n3>n1 && n3>n2) { console.log(n3)}
---
+++ lista-4/ex6.js
@@ -0,0 +1,7 @@
+let input = require('readline-sync');
+let n1 = input.questionInt("Digite um numero: ")
+let n2 = input.questionInt("Digite um numero: ")
+
+if (n1<n2) {
+    console.log(n1, n2)
+}else{console.log(n2, n1)}
