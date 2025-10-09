637df386f6b14c061f4bba30dd4c40460aa695fa
Miguel do Nascimento Evangelista <miguel.evangelista4@etec.sp.gov.br>
2025-10-09 17:13:10 -0300

    Update ex6.js

--- lista-6/ex6.js
+++ lista-6/ex6.js
@@ -1,1 +1,14 @@
 let input = require('readline-sync');
+
+let n1 = 11;
+
+while (n1>=10) {
+    n1 = input.questionInt("Digite um numero menor que 10: ");
+    if (n1<10) {
+        console.log(n1);
+    } 
+    else{
+        console.log("O numero é maior ou igual a 10");
+    }
+
+}
